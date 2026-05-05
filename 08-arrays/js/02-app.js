4e038372177eaeb02ffba3ee2310f0207d20a9f8
Angel-PC <luisangel@ciencias.unam.mx>
2026-05-04 23:34:20 -0600

    Arreglando 08 app

--- 08-arrays/js/02-app.js
+++ 08-arrays/js/02-app.js
@@ -3,5 +3,5 @@ const meses =['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio'];
 meses[0] = "primer elemento"// modificamos el valor de un arreglo 
 console.table(meses)
 
-meses [7] = 'ultimo elemento'// agregamos elemento al arreglo 
+meses[7] = 'ultimo elemento'// agregamos elemento al arreglo 
 console.table(meses)
