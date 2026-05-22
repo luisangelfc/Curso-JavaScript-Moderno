6d9149ed022d9b972ed651cfd12b1d2a83046b57
bubu-laptop <luisangel@ciencias.unam.mx>
2026-05-22 06:56:23 -0600

    continuando con la seccion de funciones JS

--- 09-functions/js/01-app.js
+++ 09-functions/js/01-app.js
@@ -3,4 +3,24 @@ function sumar(){
     console.log(2+2);
 }
 
-sumar();
+sumar(); 
+// incluso se puede mandar a llamar antes de declarar la funcion, mientras que de la otra forma, no.
+
+// expresion de funcion
+const sumar2= function(){
+    console.log(3+3);
+}
+sumar2();
+
+
+// metodos
+const num1= 20;
+const num2= "30";
+console.log(parseInt(num2)); // funcion
+console.log(num1.toString)  // metodo  
+
+function sumar3 (a,b){ //a y b sson  
+    return `el resultado es ${a+b}`
+}
+
+console.log (sumar3(2,2)); // 2, 2 son argumentos
